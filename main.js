94718ee62a3d937210f29577fc34828052e5ee76
steel25baby <bridgetgitonga73@gmail.com>
2024-05-20 17:42:29 +0300

    add a function to generate roman numbers

---
+++ main.js
@@ -0,0 +1,59 @@
+let form=document.querySelector(".input-form");
+
+function intToRoman(num) {
+  const symbolsList = [
+      ["I", 1],
+      ["IV", 4],
+      ["V", 5],
+      ["IX", 9],
+      ["X", 10],
+      ["XL", 40],
+      ["L", 50],
+      ["XC", 90],
+      ["C", 100],
+      ["CD", 400],
+      ["D", 500],
+      ["CM", 900],
+      ["M", 1000],
+      ['V\u0305', 5000],
+      ['X\u0305', 10000],
+      ['L\u0305', 50000],
+      ['C\u0305', 100000],
+      ['D\u0305', 500000],
+      ['M\u0305', 1000000],
+  ];
+
+  let i = symbolsList.length - 1;
+  let roman = "";
+  while (num > 0) {
+      const currentDivider = symbolsList[i][1];
+      const currentSymbol = symbolsList[i][0];
+      
+      const result = Math.floor(num / currentDivider);
+      if (result > 0) {
+          let temp = "";
+          for (let j = 0; j < result; j++) {
+              temp += currentSymbol;
+          }
+          roman += temp;
+          num %= currentDivider;
+      }
+      i -= 1;
+  }
+  return roman;
+}
+
+let num=document.querySelector("#number");
+let button=document.querySelector("#convert-button")
+let output=document.querySelector("#output")
+let error=document.querySelector("#error")
+
+button.addEventListener("click", function(){
+    if (num.value >0 && num.value<=4000000) {
+        let finalOutput=num.value + " = "+ intToRoman(num.value)
+        output.textContent=finalOutput   
+    } else {
+        let finalOutput="Please enter numbers between 1 and 4,000,000"
+        error.textContent=finalOutput  
+    }
+})
